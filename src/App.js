0bb296e2301d607e0021dd126ba9cf18b31f5507
intrexx mervore <146873484+numbpill3d@users.noreply.github.com>
2024-12-21 00:12:00 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LinkChipPowered from './components/LinkChipPowered';
+
+function App() {
+  return (
+    <div className="App">
+      <LinkChipPowered />
+    </div>
+  );
+}
+
+export default App;
